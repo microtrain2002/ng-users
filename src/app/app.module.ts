8d88141bea4d664d29134361fbcbee97c4029a31
chad svastisalee <swasgrafx@gmail.com>
2020-04-02 21:02:58 -0500

    Added the ability to create a user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
 import { UsersComponent } from './users/users.component';
 import { UserViewComponent } from './user-view/user-view.component';
 import { UserCreateComponent } from './user-create/user-create.component';
+import { FormsModule }   from '@angular/forms';
+
 
 @NgModule({
   declarations: [
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -8,17 +8,24 @@ const httpOptions = {
 };
 
 @Injectable(
-  {providedIn: 'root'
-})
+  {
+    providedIn: 'root'
+  })
 export class UsersService {
   private url: string = 'http://localhost:3000/api/users';
+  
   constructor(private http: HttpClient) { }
+
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.url);
   }
-  
+
   getUser(id: string): Observable<User> {
     return this.http.get<User>(`${this.url}/${id}`);
   }
-  
+
+  createUser(user: User): Observable<User> {
+    return this.http.post<User>(this.url, user, httpOptions);
+  }
+
 }

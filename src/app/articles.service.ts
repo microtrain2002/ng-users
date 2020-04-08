e8cf564d0c78a53447feb352374d057f4a609f49
chad svastisalee <swasgrafx@gmail.com>
2020-04-07 19:41:59 -0500

    Added the ability to create an article

--- src/app/articles.service.ts
+++ src/app/articles.service.ts
@@ -23,4 +23,8 @@ export class ArticlesService {
     return this.http.get<Article>(`${this.url}/${id}`);
   }
   
+  createArticle (article: Article): Observable<Article> {
+    return this.http.post<Article>(this.url, article, httpOptions);
+  }
+
 }
